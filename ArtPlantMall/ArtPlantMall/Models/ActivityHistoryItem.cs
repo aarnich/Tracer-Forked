ece3c5112849ba9b3c9ac0885a30ba7d11aa9aa1
aarn-gmpc <aaron.gmpc@gmail.com>
2020-12-12 19:55:12 +0800

    UI Polishing

--- ArtPlantMall/ArtPlantMall/Models/ActivityHistoryItem.cs
+++ ArtPlantMall/ArtPlantMall/Models/ActivityHistoryItem.cs
@@ -5,9 +5,9 @@ namespace ArtPlantMall.Models
 
     public class ActivityHistoryItem
     {
-        public string Content { get; set; } = "";
-        public string Date { get; set; } = "";
-        public string Time { get; set; } = "";
+        public string Content { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
         public List<LocationSafety> LocationSafety { get; set; }
 
     }
