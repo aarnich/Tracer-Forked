77b43b077ede6b719c230d9d739953ca55f5088d
aarn-gmpc <aaron.gmpc@gmail.com>
2020-12-13 21:33:06 +0800

    Delete Activity Method

--- ArtPlantMall/ArtPlantMall/Views/Templates/ExpandedBasketItemTemplate.xaml.cs
+++ ArtPlantMall/ArtPlantMall/Views/Templates/ExpandedBasketItemTemplate.xaml.cs
@@ -1,12 +1,25 @@
-﻿using Xamarin.Forms;
+﻿using ArtPlantMall.Models;
+using ArtPlantMall.Services;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace ArtPlantMall.Views.Templates
 {
-    public partial class ExpandedBasketItemTemplate : ContentView
+	public partial class ExpandedBasketItemTemplate : ContentView
 	{
-		public ExpandedBasketItemTemplate ()
+		public ExpandedBasketItemTemplate()
 		{
 			InitializeComponent();
+			var ActivityID = ID.Text;
+
+		}
+		private async Task DeleteActivity(object sender, EventArgs e)
+		{
+			Activity act = (Activity)sender;
+			var ID = act.ActivityId;
+			FirebaseService _firebase = new FirebaseService();
+            await _firebase.DeleteActivity(ID);
 		}
 	}
 }
