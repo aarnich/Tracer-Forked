c2766fbbc8bf4dc2bc8c5419ff4d8ca1d7364e6a
aarn-gmpc <aaron.gmpc@gmail.com>
2020-12-13 17:23:46 +0800

    CovidStatistics Fix

--- ArtPlantMall/ArtPlantMall/Views/CovidStats.xaml.cs
+++ ArtPlantMall/ArtPlantMall/Views/CovidStats.xaml.cs
@@ -14,6 +14,18 @@ namespace ArtPlantMall.Views
         public CovidStats()
         {
             InitializeComponent();
+            var browser = new WebView();
+            var htmlSource = new HtmlWebViewSource();
+            const string quote = "\""; 
+            htmlSource.Html = @"<html><body>" +
+            "<h1></h1>" +
+              "<p></p>" +
+"<script src=" + quote + "https://apps.elfsight.com/p/platform.js" + quote +
+            "defer></script>" +
+"<div class=" + quote+ "elfsight-app-ecb58423-a880-4350-aa92-bf49c73866be" + quote + "></div>" +  
+              "</body></html>";
+            browser.Source = htmlSource;
+            BrowserGrid.Children.Add(browser);
         }
     }
 }
